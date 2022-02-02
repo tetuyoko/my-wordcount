4c27cf60dbfe74a79c47b5aeeb85f87983d74316
tetuyoko <tyokoyama53@gmail.com>
2022-02-02 18:14:29 +0900

    Impl line test

---
+++ tests/lines.rs
@@ -0,0 +1,33 @@
+use my_wordcount::{count, CountOption};
+use std::io::Cursor;
+
+#[macro_use]
+mod utils;
+
+#[test]
+fn line_count_works() {
+    let input = Cursor::new(
+        r#"Tokyo, Japan
+Kyoto, Japan
+Tokyo, Japan
+ShangHai, China
+"#,
+    );
+
+    let freq = count(input, CountOption::Line);
+    assert_map!(freq, {
+        "Tokyo, Japan" => 2,
+        "Kyoto, Japan" => 1,
+        "ShangHai, China" => 1
+    });
+}
+#[test]
+fn line_count_lfcr() {
+    let input = Cursor::new("aa\r\nbb\r\ncc\r\nbb");
+    let freq = count(input, CountOption::Line);
+    assert_map!(freq, {
+        "aa" => 1,
+        "bb" => 2,
+        "cc" => 1
+    });
+}
